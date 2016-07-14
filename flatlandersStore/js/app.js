3058513387f38ea0a3e6fd713696b93d7583afac
Alonso S <alonsosalcido13@gmail.com>
2016-07-14 02:41:58 -0600

    Using logic in the controller Panel, chenge all the html elements for scope elementes in this controller

--- flatlandersStore/js/app.js
+++ flatlandersStore/js/app.js
@@ -5,6 +5,20 @@
 		this.products = gems1;
 	});
 
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+
+
+	})
+
 	var gems = [
 		{
 			name: 'Dodecahedron',
