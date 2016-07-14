00a865f52491a9f9fcb0e600384fc3fbefc02fd2
Alonso S <alonsosalcido13@gmail.com>
2016-07-14 01:05:12 -0600

    ng-show with canPurchase

--- flatlandersStore/js/app.js
+++ flatlandersStore/js/app.js
@@ -2,13 +2,14 @@
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem1;
+		this.product = gem;
 	});
 
 	var gem = {
 		name: 'Dodecahedron',
 		price: 2.95,
 		description: 'Lorem ipsum dolor sit amet',
+		canPurchase: true,
 	}
 
 	var gem1 = {
