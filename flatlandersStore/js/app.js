f08181dd644ae258dbf35880794e7ba4728b0ac4
Alonso S <alonsosalcido13@gmail.com>
2016-07-14 01:14:24 -0600

    ng-repeat expression with array of elements

--- flatlandersStore/js/app.js
+++ flatlandersStore/js/app.js
@@ -2,16 +2,25 @@
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: 'Lorem ipsum dolor sit amet',
-		canPurchase: true,
-		soldOut: true,
-	}
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: 'Lorem ipsum dolor sit amet',
+			canPurchase: true,
+			soldOut: true,
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: 'Lorem ipsum dolor sit amet',
+			canPurchase: true,
+		}
+
+	];
 
 	var gem1 = {
 		name: 'Azurite',
