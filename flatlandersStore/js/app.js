84956016387be627b3759d9cea6fd1eb49e0173d
Alonso S <alonsosalcido13@gmail.com>
2016-07-14 01:01:55 -0600

    Second challenge, controller and gems inside the html

--- flatlandersStore/js/app.js
+++ flatlandersStore/js/app.js
@@ -2,12 +2,17 @@
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.product = gem1;
 	});
 
 	var gem = {
 		name: 'Dodecahedron',
 		price: 2.95,
 		description: 'Lorem ipsum dolor sit amet',
+	}
+
+	var gem1 = {
+		name: 'Azurite',
+		price: 2.95
 	}
 })();
